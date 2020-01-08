0bc30c52eea9788c7b16e42861b39400afd083aa
a64070ho <harry.obrien@student.manchester.ac.uk>
2020-01-08 00:30:28 +0000

    fix duplicated output on uart

--- kernel.c
+++ kernel.c
@@ -9,29 +9,34 @@
 #include <stddef.h>
 #include <stdint.h>
 
-static inline void mmio_write(uint32_t reg, uint32_t data) {
-    //dereference the reg, and store the data at the given location
-    *(volatile uint32_t*) reg = data;
+static inline void mmio_write(uint32_t reg, uint32_t data)
+{
+    *(volatile uint32_t*)reg = data;
 }
 
-static inline uint32_t mmio_read(uint32_t reg) {
-    return *(volatile uint32_t*) reg;
+static inline uint32_t mmio_read(uint32_t reg)
+{
+    return *(volatile uint32_t*)reg;
 }
 
-// loop <delay> times in a way that the compiler won't optimise away
-static inline void delay(uint32_t count) {
-    asm volatile("__delay_%=: subs %[count], %[count], #1; bne __delay_%=\n" : "=r"(count): [count]"0"(count) : "cc");
+// Loop <delay> times in a way that the compiler won't optimize away
+static inline void delay(int32_t count)
+{
+    asm volatile("__delay_%=: subs %[count], %[count], #1; bne __delay_%=\n"
+            : "=r"(count): [count]"0"(count) : "cc");
 }
 
-enum {
-    //GPIO registers base addr
-    GPIO_BASE = 0x3F200000,
+enum
+{
+    // The GPIO registers base address.
+    GPIO_BASE = 0x3F200000, // for raspi2 & 3, 0x20200000 for raspi1
 
     GPPUD = (GPIO_BASE + 0x94),
-    GPPUDCLK0 = (GPIO_BASE + 98),
+    GPPUDCLK0 = (GPIO_BASE + 0x98),
 
-    // Base address for UART
-    UART0_BASE = 0x3F201000,
+    // The base address for UART.
+    UART0_BASE = 0x3F201000, // for raspi2 & 3, 0x20201000 for raspi1
+
     UART0_DR     = (UART0_BASE + 0x00),
     UART0_RSRECR = (UART0_BASE + 0x04),
     UART0_FR     = (UART0_BASE + 0x18),
@@ -49,19 +54,20 @@ enum {
     UART0_ITCR   = (UART0_BASE + 0x80),
     UART0_ITIP   = (UART0_BASE + 0x84),
     UART0_ITOP   = (UART0_BASE + 0x88),
-    UART0_TDR    = (UART0_BASE + 0x8C)
+    UART0_TDR    = (UART0_BASE + 0x8C),
 };
 
-void uart_init() {
-    mmio_write(UART0_CR, 0);
+void uart_init()
+{
+    mmio_write(UART0_CR, 0x00000000);
 
-    mmio_write(GPPUD, 0);
+    mmio_write(GPPUD, 0x00000000);
     delay(150);
 
     mmio_write(GPPUDCLK0, (1 << 14) | (1 << 15));
     delay(150);
 
-    mmio_write(GPPUDCLK0, 0);
+    mmio_write(GPPUDCLK0, 0x00000000);
 
     mmio_write(UART0_ICR, 0x7FF);
 
@@ -70,39 +76,40 @@ void uart_init() {
 
     mmio_write(UART0_LCRH, (1 << 4) | (1 << 5) | (1 << 6));
 
-    mmio_write(UART0_IMSC, (1 << 1) | (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7) | (1 << 8) | (1 << 9) | (1 << 10));
+    mmio_write(UART0_IMSC, (1 << 1) | (1 << 4) | (1 << 5) | (1 << 6) |
+            (1 << 7) | (1 << 8) | (1 << 9) | (1 << 10));
 
-    mmio_write(UART0_CR, (1) | (1 << 8) | (1 << 9));
+    mmio_write(UART0_CR, (1 << 0) | (1 << 8) | (1 << 9));
 }
 
-void uart_putc(unsigned char c) {
-    //wait until uart is ready
-    while(mmio_read(UART0_FR) & (1 << 5)) {}
-
-    //write char to uart
+void uart_putc(unsigned char c)
+{
+    while ( mmio_read(UART0_FR) & (1 << 5) ) { }
     mmio_write(UART0_DR, c);
 }
 
-unsigned char uart_getc() {
-    while(mmio_read(UART0_FR) & (1 << 4)) {}
+unsigned char uart_getc()
+{
+    while ( mmio_read(UART0_FR) & (1 << 4) ) { }
     return mmio_read(UART0_DR);
 }
 
-void uart_puts(const char* str) {
-    for(size_t i = 0; str[i] != '\0'; i++)
-        uart_putc(str[i]);
+void uart_puts(const char* str)
+{
+    for (size_t i = 0; str[i] != '\0'; i ++)
+        uart_putc((unsigned char)str[i]);
 }
 
-void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags) {
+void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags)
+{
     (void) r0;
     (void) r1;
-    (void) atags; //pointer to the atags (hardware and cmd line info)
+    (void) atags;
 
     uart_init();
-    delay(150);
-    uart_puts("Hello, kernel world!\r\n");
+    uart_puts("Hello, kernel World!\r\n");
 
-    while(1) {
+    while (1) {
         uart_putc(uart_getc());
         uart_putc('\n');
     }
